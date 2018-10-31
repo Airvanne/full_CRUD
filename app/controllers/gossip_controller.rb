d174d56cc9e517f85d5f6285a9050c8902c527ba
Airvanne <erwanb@lilo.org>
2018-11-01 00:03:35 +0100

    update réussie

--- app/controllers/gossip_controller.rb
+++ app/controllers/gossip_controller.rb
@@ -4,27 +4,37 @@ class GossipController < ApplicationController
   end
 
   def create
+    # gossip = Gossip.create!(params_from_form)
     @gossip = Gossip.new
     @gossip.anonymous_gossiper = params[:username]
     @gossip.title = params[:title]
     @gossip.content = params[:content]
     @gossip.save
+    redirect_to "/gossip/#{params[:id]}"
   end
 
   def show
+    # @gossip = Gossip.find(params[:id])
+  end
+
+  def index
     @allgossips = Gossip.all.count
   end
 
+
   def edit
-    @params = params[:id]
+    # @params = params[:id]
+    puts 'SAAALUUUUUUTTTT'
+    @gossip = Gossip.find(params[:id])
   end
 
   def update
-puts  @params = params[:id]
-   @g = Gossip.find(@params)
-   @g.anonymous_gossiper = params[:username]
-   @g.title = params[:title]
-   @g.content = params[:content]
-   @g.save
+    @gossip = Gossip.find(params[:id])
+    @gossip.anonymous_gossiper = params[:gossip][:anonymous_gossiper]
+    @gossip.title = params[:gossip][:title]
+    @gossip.content = params[:gossip][:content]
+    @gossip.save
+    redirect_to "/gossip/#{params[:id]}"
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :gossips
-
-  get '/', to: 'static_pages#home'
-
-  get 'gossip/new', to: 'gossip#new'
-  post 'gossip/create', to: 'gossip#create'
-  get 'gossip/create/:id', to: 'gossip#id'
-
-  get 'gossip/show', to: 'gossip#show'
-
-
-  get 'gossip/:id/edit', to: 'gossip#edit'
-  patch 'gossip/:id', to: 'gossip#update'
-
-  get 'gossip/:id', to: 'gossip#show'
+  get 'gossip/new'
+  root 'static_pages#home'
+  resources 'gossip'
 
 end
